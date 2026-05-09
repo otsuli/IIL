139fedc627a9a9f43d95c661f3b9bbc45fa61067
boom1401 <otso.reijonen@icloud.com>
2026-05-09 21:47:12 +0300

    added leftChild and righChild declarations

--- src/parser/parsing.cpp
+++ src/parser/parsing.cpp
@@ -14,17 +14,13 @@ std::vector<ASTnode> numericalExprAST::NumExprAST(std::vector<Token> &Tokens)
         curTok = Tokens[i];
         if (parserUtils::isBinary(curTok.value))
         {
-
+            curTok.rightChild = Tokens[i - 1]; 
             parserUtils::buffer_Op(curTok);
+            curTok.leftChild = Tokens[i + 1]; 
         }
     }
     Token highest_precedence_op = parserUtils::getHighestPrecedence(parserUtils::opBuffer);
     std::shared_ptr<ASTnode> highest_precedence_node = parserUtils::to_node(highest_precedence_op);
 
     parserUtils::rem_buffered_op(highest_precedence_op);
-
-    //? IDEA: Create OpTok object that holds number nodes as it's children. That way we can easily convert it to a node and connect thme to each other.
-    //? OpTok can be inherited from the Token class. And it could have the property of children. And maybe the constructors could auto assign a precedence to it?.
-
-    
 }
