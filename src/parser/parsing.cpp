c91b2c1f6625fedae32fa9809d69551002a671c1
boom1401 <otso.reijonen@icloud.com>
2026-05-15 21:11:02 +0300

    starting the parser from scratch cuz it was O(n^2) before I need to make it O(N) so I gotta implement a LL(1) parser.

--- src/parser/parsing.cpp
+++ src/parser/parsing.cpp
@@ -5,35 +5,4 @@
 #include <memory>
 // Example: 1 + 1. Tokens are '1' '+' and '1'
 
-std::vector<std::shared_ptr<ASTNode>> numericalExprAST::connect_nodes(std::vector<OpTok> opTokens)
-{
-    for (int i = 0; i <= size(opTokens); i++)
-    {
-        parserUtils::operatorBuffer buff;
-        OpTok highest_precedence_op = parserUtils::getHighestPrecedence(buff.getBuffer());
-        std::shared_ptr<ASTNode> highest_precedence_node = parserUtils::to_node_op(highest_precedence_op);
-
-        buff.rem_buffered_op(highest_precedence_op);
-    }
-}
-
-std::vector<ASTNode> numericalExprAST::NumExprAST(std::vector<Token> &Tokens)
-{
-    parserUtils::operatorBuffer buff;
-    buff.clearBuffer();
-
-    OpTok curTok;
-    for (int i = 0; i <= Tokens.size(); i++)
-    {
-        curTok = Tokens[i];
-        if (parserUtils::isBinary(curTok.value))
-        {
-            std::unique_ptr<Token> unique_ptr_left = std::make_unique<Token>(Tokens[i - 1]);
-            std::unique_ptr<Token> unique_ptr_right = std::make_unique<Token>(Tokens[i + 1]);
-
-            curTok.rightChild = std::make_unique<OpTok>(unique_ptr_left, left_child_t{}); // Heap allocation
-            curTok.rightChild = std::make_unique<OpTok>(unique_ptr_left, left_child_t{});
-            buff.buffer_Op(curTok);
-        }
-    }
-}
+//! Start from scratch *CRY*
