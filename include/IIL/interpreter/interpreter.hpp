7a2353cb6364758723e8c0dbb201154bb1b16265
boom1401 <otso.reijonen@icloud.com>
2026-05-22 19:44:28 +0300

    create interpreter class and evaluation methods for unary grouping and literal expressions

---
+++ include/IIL/interpreter/interpreter.hpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <variant>
+#include "parser/expr.hpp"
+
+// Object type is used to store multiple different datatypes
+// so we can make returned values objects of any type.
+
+using Object = std::variant<double, std::string, bool, std::nullptr_t, Token,
+                            Expr, UnaryExpr, Grouping, Literal>;
+
+class Interpreter {
+    Object evaluate(Expr expr);
+
+   public:
+    Object visitLiteralExpr(Literal expr);
+    Object visitGroupingExpr(Grouping expr);
+    Object visitUnaryExpr(UnaryExpr expr);
+};
