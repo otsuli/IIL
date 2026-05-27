82699599f5fc28878f6facc41c48edb10a8c010c
boom1401 <otso.reijonen@icloud.com>
2026-05-27 08:52:47 +0300

    update test commands

--- scripts/tests/build_lexer_test.cmd
+++ scripts/tests/build_lexer_test.cmd
@@ -3,6 +3,6 @@ echo "building REPL"
 REM -S . means the source directory is the current folder
 REM -B  means put the generated build files the build/ folder
 
-cmake -S .. -B build 
-cmake --build build --target IIL_lexer_test -DCMAKE_BUILD_TYPE=Debug
+cmake -DCMAKE_BUILD_TYPE=Debug -S .. -B build 
+cmake --build build --target IIL_lexer_test 
 ctest --test-dir build --output-on-failure
