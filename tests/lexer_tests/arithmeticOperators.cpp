16be5d053a67a7ff903637699500600a3caa5e63
boom1401 <otso.reijonen@icloud.com>
2026-05-31 17:09:54 +0300

    update testing to include dual character operators

--- tests/lexer_tests/arithmeticOperators.cpp
+++ tests/lexer_tests/arithmeticOperators.cpp
@@ -118,5 +118,28 @@ TEST(lexerTests, HandlesModuloOperator) {
     tokenizing lexer;
 
     EXPECT_EQ(lexer.tokenize(op), *expectedOutput)
-        << "Handles Mutliplication Operators test failed.";
+        << "Handles Modulo Operators test failed.";
+}
+
+TEST(lexerTests, HandlesFloorDivOperator) {
+    // GDB COMMANDS :/ (for when you need to debug *cry* )
+    // break LexerTest_BasicTokens_Test::TestBody
+    // run --gtest_filter=lexerTests.HandlesFloorDivOperator
+
+    const auto op = std::make_unique<std::string>("//");
+    const u16 expected_line = 1;
+    const u16 expected_column = 1;
+    Token tok = Token::make_token(TokenType::FloorDiv, *op, expected_line,
+                                  expected_column);
+    Token EOFTok = Token::make_token(TokenType::FileEnd, "fileEnd",
+                                     expected_line + 1, expected_column);
+    const std::unique_ptr<std::vector<Token>> expectedOutput =
+        std::make_unique<std::vector<Token>>();
+
+    expectedOutput->push_back(tok);
+    expectedOutput->push_back(EOFTok);
+    tokenizing lexer;
+
+    EXPECT_EQ(lexer.tokenize(op), *expectedOutput)
+        << "Handles FloorDiv Operators test failed.";
 }
--- tests/lexer_tests/comparisonOperators.cpp
+++ tests/lexer_tests/comparisonOperators.cpp
@@ -32,7 +32,7 @@ TEST(lexerTests, HandlesGreaterOperator) {
 
 TEST(lexerTests, HandlesLessOperator) {
     // GDB COMMANDS :/ (for when you need to debug *cry* )
-    // break LexerTest_BasicTokens_Test::TestBody
+    // break LexerTest_BasicTokens_Test::TestBody()
     // run --gtest_filter=lexerTests.HandlesLessOperator
 
     const auto op = std::make_unique<std::string>("<");
@@ -55,7 +55,7 @@ TEST(lexerTests, HandlesLessOperator) {
 
 TEST(lexerTests, HandlesEqualOperator) {
     // GDB COMMANDS :/ (for when you need to debug *cry* )
-    // break LexerTest_BasicTokens_Test::TestBody
+    // break LexerTest_BasicTokens_Test::TestBody()
     // run --gtest_filter=lexerTests.HandlesEqualOperator
 
     const auto op = std::make_unique<std::string>("=");
@@ -74,4 +74,96 @@ TEST(lexerTests, HandlesEqualOperator) {
 
     EXPECT_EQ(lexer.tokenize(op), *expectedOutput)
         << "Handles Equal Operators test failed.";
+}
+
+TEST(lexerTests, HandlesEqualEqualOperator) {
+    // GDB COMMANDS :/ (for when you need to debug *cry* )
+    // break LexerTest_BasicTokens_Test::TestBody()
+    // run --gtest_filter=lexerTests.HandlesEqualEqualOperator
+
+    const auto op = std::make_unique<std::string>("==");
+    const u16 expected_line = 1;
+    const u16 expected_column = 1;
+
+    Token tok = Token::make_token(TokenType::EqualEqual, *op, expected_line,
+                                  expected_column);
+    Token EOFTok = Token::make_token(TokenType::FileEnd, "fileEnd",
+                                     expected_line + 1, expected_column);
+    const std::unique_ptr<std::vector<Token>> expectedOutput =
+        std::make_unique<std::vector<Token>>();
+    expectedOutput->push_back(tok);
+    expectedOutput->push_back(EOFTok);
+    tokenizing lexer;
+
+    EXPECT_EQ(lexer.tokenize(op), *expectedOutput)
+        << "Handles EqualEqual Operators test failed.";
+}
+
+TEST(lexerTests, HandlesNotEqualOperator) {
+    // GDB COMMANDS :/ (for when you need to debug *cry* )
+    // break LexerTest_BasicTokens_Test::TestBody()
+    // run --gtest_filter=lexerTests.HandlesNotEqualOperator
+
+    const auto op = std::make_unique<std::string>("!=");
+    const u16 expected_line = 1;
+    const u16 expected_column = 1;
+
+    Token tok = Token::make_token(TokenType::BangEqual, *op, expected_line,
+                                  expected_column);
+    Token EOFTok = Token::make_token(TokenType::FileEnd, "fileEnd",
+                                     expected_line + 1, expected_column);
+    const std::unique_ptr<std::vector<Token>> expectedOutput =
+        std::make_unique<std::vector<Token>>();
+    expectedOutput->push_back(tok);
+    expectedOutput->push_back(EOFTok);
+    tokenizing lexer;
+
+    EXPECT_EQ(lexer.tokenize(op), *expectedOutput)
+        << "Handles NotEqual Operators test failed.";
+}
+
+TEST(lexerTests, HandlesGreaterEqualOperator) {
+    // GDB COMMANDS :/ (for when you need to debug *cry* )
+    // break LexerTest_BasicTokens_Test::TestBody()
+    // run --gtest_filter=lexerTests.HandlesGreaterEqualOperator
+
+    const auto op = std::make_unique<std::string>(">=");
+    const u16 expected_line = 1;
+    const u16 expected_column = 1;
+
+    Token tok = Token::make_token(TokenType::GreaterEqual, *op, expected_line,
+                                  expected_column);
+    Token EOFTok = Token::make_token(TokenType::FileEnd, "fileEnd",
+                                     expected_line + 1, expected_column);
+    const std::unique_ptr<std::vector<Token>> expectedOutput =
+        std::make_unique<std::vector<Token>>();
+    expectedOutput->push_back(tok);
+    expectedOutput->push_back(EOFTok);
+    tokenizing lexer;
+
+    EXPECT_EQ(lexer.tokenize(op), *expectedOutput)
+        << "Handles greater equal Operators test failed.";
+}
+
+TEST(lexerTests, HandlesLessEqualOperator) {
+    // GDB COMMANDS :/ (for when you need to debug *cry* )
+    // break LexerTest_BasicTokens_Test::TestBody()
+    // run --gtest_filter=lexerTests.HandlesLessEqualOperator
+
+    const auto op = std::make_unique<std::string>("<=");
+    const u16 expected_line = 1;
+    const u16 expected_column = 1;
+
+    Token tok = Token::make_token(TokenType::LessEqual, *op, expected_line,
+                                  expected_column);
+    Token EOFTok = Token::make_token(TokenType::FileEnd, "fileEnd",
+                                     expected_line + 1, expected_column);
+    const std::unique_ptr<std::vector<Token>> expectedOutput =
+        std::make_unique<std::vector<Token>>();
+    expectedOutput->push_back(tok);
+    expectedOutput->push_back(EOFTok);
+    tokenizing lexer;
+
+    EXPECT_EQ(lexer.tokenize(op), *expectedOutput)
+        << "Handles less equal Operators test failed.";
 }
